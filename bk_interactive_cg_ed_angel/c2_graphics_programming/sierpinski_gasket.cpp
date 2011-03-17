70206b81854aae8a37cbb78b73bfb911e208ce4b
Deepak Antony <deepakantony@Deepak-Antonys-MacBook-Pro.local>
2011-03-17 00:55:22 -0700

    was able to compile the initial part

--- bk_interactive_cg_ed_angel/c2_graphics_programming/sierpinski_gasket.cpp
+++ bk_interactive_cg_ed_angel/c2_graphics_programming/sierpinski_gasket.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+#else
 #include <GL/gl.h>
 #include <GL/glut.h>
+#endif
 
 using namespace std;
 
+void display() {
+}
+
+void init() {
+}
+
 int main(int argc, char **argv) {
-	return 0;
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+  glutInitWindowSize(500, 500);
+  glutInitWindowPosition(0,0);
+  glutCreateWindow("Sierpinski gasket program");
+
+  glutDisplayFunc(display);
+  init();
+  glutMainLoop();
+  
+  return 0;
 }
