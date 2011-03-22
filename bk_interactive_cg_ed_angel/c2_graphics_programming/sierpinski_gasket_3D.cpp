d97782dad7cd15c290b3bdbbd22e34bb6fd9209b
Deepak <deepakantony@gmail.com>
2011-03-22 01:06:59 -0700

    created the file

---
+++ bk_interactive_cg_ed_angel/c2_graphics_programming/sierpinski_gasket_3D.cpp
@@ -0,0 +1,18 @@
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/gl.h>
+#include <GL/glut.h>
+#endif
+
+void display() {
+}
+
+void init() {
+}
+
+int main(int argc, char **argv) {
+  
+  return 0;
+}
